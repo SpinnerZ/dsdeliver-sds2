6181f6588dfdef8e3d0028d400b81672aee293f1
Leonardo A. de Souza <leo26gpl@gmail.com>
2021-01-07 18:17:31 -0300

    C:/Program Files/Git/GET orders

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/controller/OrderController.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dsdeliver.controller;
+
+import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+// Cria o controlador Rest no SpringBoot
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderController {
+
+    // Controller depende do service e o service depende do repository
+    @Autowired
+    private OrderService service;
+
+    @GetMapping
+    public ResponseEntity<List<OrderDTO>> findAll() {
+        List<OrderDTO> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
